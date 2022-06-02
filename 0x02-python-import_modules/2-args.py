a788311e5e8d9bf32c1ee9bc82552b24685a6380
vikkybass1 <vikkybass1@gmail.com>
2022-06-02 17:52:51 +0100

    Latest Commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    from sys import argv
-    userin = argv[1:]
-    size = len(userin)
-    print("{:d} {:s}{:s}".
-          format(size,
-                 "arguments" if (size) is not 1 else "argument",
-                 "." if (size) is 0 else ":"))
-    for idx, arg in enumerate(userin):
-        print("{:d}: {:s}".format(idx + 1, arg))
+if __name == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    arg_num = len(argv)
+    i = 1
+    if arg_num == 0:
+        print("{:d} arguments.".format(arg_num))
+    elif arg_num == 1:
+        print("{:d} argument.".format(arg_num))
+        print("{:d}: {:s}".format(i, sys.argv[1]))
+    else:
+        print("{:d} arguments.".format(arg_num))
+        while i < arg_num:
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+            i += 1
